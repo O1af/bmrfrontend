e7028fbb73032cae3fc81b2f6f919856711d9e29
Olaf Dsouza <olafdsouza27@gmail.com>
2022-05-31 22:10:45 -0400

    fixed site

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,30 +1,19 @@
+const calculateRate = (sex, weight, height, age) => {
+	let bmr = 0;
+	if (sex == "m") {
+		bmr = 66.47 + 6.24 * weight + 12.7 * height - 6.755 * age;
+	} else {
+		bmr = 655.1 + 6.24 * weight + 4.7 * height - 4.7 * age;
+	}
+	return bmr;
+};
+
 const handleSubmit = async () => {
 	weight = document.getElementById("weight").value;
 	height = document.getElementById("height").value;
 	age = document.getElementById("age").value;
 	sex = document.getElementById("sex").value;
 	result = document.getElementById("bmrresult");
-	fetch("https://anishisreddy-95-hytpdimfwa-uc.a.run.app/bmr", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			sex: sex,
-			height: height,
-			weight: weight,
-			age: age,
-		}),
-	})
-		.then((response) => {
-			response = response.json();
-			return response;
-		})
-		.then((res) => {
-			console.log(res);
-			result.innerHTML = "Basal Metabolic Rate: " + res + " Calories";
-		})
-		.catch((err) => {
-			console.error(err);
-		});
+	res = calculateRate(sex,weight,height,age);
+	result.innerHTML = "Basal Metabolic Rate: " + res + " Calories";
 };
